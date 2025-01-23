c02e39a680ffdfded3589a771a26fd47c2596839
tdgao <mr.trumgao@gmail.com>
2025-01-22 21:23:50 -0800

    add web ui, set up svelte with vite

---
+++ services/web-ui/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <h1>Trade Simple</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  main {
+    max-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+</style>

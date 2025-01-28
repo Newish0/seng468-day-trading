ceffd02d6bc52e84c3bc900018c4634b810336e4
gawlster <connorgawley@gmail.com>
2025-01-28 00:24:50 -0800

    feat: add type validation to post requests

--- services/user-api/src/controllers/engineController.ts
+++ services/user-api/src/controllers/engineController.ts
@@ -1,11 +1,26 @@
-import type { Context } from "hono";
+import type { Context, Env } from "hono";
+import type { CancelStockTransactionRequest } from "shared-types/dtos/user-api/engine/cancelStockTransaction";
+import type { PlaceStockOrderRequest } from "shared-types/dtos/user-api/engine/placeStockOrder";
+import type { WrappedInput } from "shared-types/hono";
 
 const engineController = {
-  placeStockOrder: async (c: Context) => {
+  placeStockOrder: async <
+    E extends Env,
+    P extends string,
+    I extends WrappedInput<PlaceStockOrderRequest>
+  >(
+    c: Context<E, P, I>
+  ) => {
     c.status(400);
     return c.json({ message: "Not implemented" });
   },
-  cancelStockTransaction: async (c: Context) => {
+  cancelStockTransaction: async <
+    E extends Env,
+    P extends string,
+    I extends WrappedInput<CancelStockTransactionRequest>
+  >(
+    c: Context<E, P, I>
+  ) => {
     c.status(400);
     return c.json({ message: "Not implemented" });
   },
--- services/user-api/src/routes/engineRoutes.ts
+++ services/user-api/src/routes/engineRoutes.ts
@@ -1,11 +1,19 @@
 import { Hono } from "hono";
+import { isCancelStockTransactionRequest } from "shared-types/dtos/user-api/engine/cancelStockTransaction";
+import { isPlaceStockOrderRequest } from "shared-types/dtos/user-api/engine/placeStockOrder";
+import { getValidator } from "shared-utils";
 import engineController from "../controllers/engineController";
 
 const engineRoutes = new Hono();
 
-engineRoutes.post("/placeStockOrder", engineController.placeStockOrder);
+engineRoutes.post(
+  "/placeStockOrder",
+  getValidator(isPlaceStockOrderRequest),
+  engineController.placeStockOrder
+);
 engineRoutes.post(
   "/cancelStockTransaction",
+  getValidator(isCancelStockTransactionRequest),
   engineController.cancelStockTransaction
 );
 

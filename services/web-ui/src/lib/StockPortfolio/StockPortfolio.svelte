9498565c809fdcc6eec5956aa72d9b07c4f3e502
tdgao <mr.trumgao@gmail.com>
2025-01-29 15:39:14 -0800

    add stock portfolio

--- services/web-ui/src/lib/StockPortfolio/StockPortfolio.svelte
+++ services/web-ui/src/lib/StockPortfolio/StockPortfolio.svelte
@@ -1,1 +1,47 @@
-<h2>Stock portfolio</h2>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let stocks: any;
+
+  const getStocks = () => {
+    // to be implemented
+
+    return [
+      {
+        stock: "AAPL",
+        quantity: 5,
+      },
+      {
+        stock: "GOOGL",
+        quantity: 5,
+      },
+    ];
+  };
+
+  onMount(() => {
+    stocks = getStocks();
+  });
+</script>
+
+<div class="flex flex-col w-full">
+  <h3>Holdings</h3>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Stock</th>
+        <th>Quantity</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each stocks as transaction}
+        <tr>
+          <td>{transaction.stock}</td>
+          <td>{transaction.quantity}</td>
+          <td><button>Sell stock</button></td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>

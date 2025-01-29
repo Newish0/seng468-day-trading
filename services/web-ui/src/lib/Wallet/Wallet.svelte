467b701f90968ed4eb1cde4d1b11c3ce056b051d
tdgao <mr.trumgao@gmail.com>
2025-01-29 15:26:01 -0800

    add wallet

--- services/web-ui/src/lib/Wallet/Wallet.svelte
+++ services/web-ui/src/lib/Wallet/Wallet.svelte
@@ -1,2 +1,30 @@
-<h2>Wallet</h2>
-<p>Balance: $1234</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let balance: number;
+
+  onMount(() => {
+    balance = getBalance();
+  });
+
+  const getBalance = (): number => {
+    return 12345;
+  };
+
+  const handleAddMoney = () => {
+    // to be implemented
+  };
+</script>
+
+<div class="flex flex-col w-full">
+  <h3>Wallet</h3>
+
+  {#if !balance}
+    Loading...
+  {:else}
+    <div class="flex justify-between items-center">
+      <p>Balance: ${balance}</p>
+      <button on:click={handleAddMoney}>Add money</button>
+    </div>
+  {/if}
+</div>

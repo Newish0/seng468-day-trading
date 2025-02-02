2d97e72fb4d817187a574ec4ef2a76ec6496df8a
tdgao <mr.trumgao@gmail.com>
2025-02-01 23:45:02 -0800

    fix config for user-api reverse proxy

--- services/user-api/src/index.ts
+++ services/user-api/src/index.ts
@@ -3,7 +3,7 @@ import engineRoutes from "./routes/engineRoutes.ts";
 import setupRoutes from "./routes/setupRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 
-const port = 8080; //Bun.env.PORT || 3000;
+const port = Bun.env.PORT || 3000;
 const app = new Hono();
 
 app.route("/transaction", transactionRoutes);
@@ -13,6 +13,11 @@ app.route("/setup", setupRoutes);
 Bun.serve({
   fetch: app.fetch,
   port,
+});
+
+app.get("/", async (c: any) => {
+  c.status(400);
+  return c.json({ message: "Not implemented" });
 });
 
 console.log(`Server running on port ${port}`);

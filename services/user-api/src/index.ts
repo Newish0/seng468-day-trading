239ceaf2eeb1d95e26f93f491bc9b45ff5df5821
tdgao <mr.trumgao@gmail.com>
2025-02-01 21:44:07 -0800

    add in progress user-api docker file and nginx.conf

--- services/user-api/src/index.ts
+++ services/user-api/src/index.ts
@@ -3,7 +3,7 @@ import engineRoutes from "./routes/engineRoutes.ts";
 import setupRoutes from "./routes/setupRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 
-const port = Bun.env.PORT || 3000;
+const port = 8080; //Bun.env.PORT || 3000;
 const app = new Hono();
 
 app.route("/transaction", transactionRoutes);

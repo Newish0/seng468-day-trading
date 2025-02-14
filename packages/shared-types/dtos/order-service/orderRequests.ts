64f47eae1c922ff3a8f8ce813ca4ab3bdd3a7fe1
Sai Bannur <saibannur@uvic.ca>
2025-02-13 17:55:28 -0800

    added stock_name to /getStockPrices

--- packages/shared-types/dtos/order-service/orderRequests.ts
+++ packages/shared-types/dtos/order-service/orderRequests.ts
@@ -1,23 +1,28 @@
 export type LimitSellOrderRequest = {
-    stock_id: string;
-    quantity: number;
-    price: number;
-    stock_tx_id: string;
-    user_name: string;
+  stock_id: string;
+  quantity: number;
+  price: number;
+  stock_tx_id: string;
+  user_name: string;
 };
 
 export type MarketBuyRequest = {
-    stock_id: string;
-    quantity: number;
-    stock_tx_id: string;
-    budget: number;
-    user_name: string;
+  stock_id: string;
+  quantity: number;
+  stock_tx_id: string;
+  budget: number;
+  user_name: string;
 };
 
 export type CancelSellRequest = {
-    stock_id: string;
-    quantity: number;
-    price: number;
-    stock_tx_id: string;
+  stock_id: string;
+  quantity: number;
+  price: number;
+  stock_tx_id: string;
 };
 
+// Response from Matching Engine for /stockPrices
+export type StockPricesResponse = {
+  stock_id: string;
+  current_price: number;
+};

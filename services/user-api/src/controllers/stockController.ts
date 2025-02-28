d3c022294d33a61f69925a8d92b8828e059096ed
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-02-27 19:47:05 -0800

    feat(User API): removed get stock prices endpoint
    get stock prices is now handled by the new stock price service.

--- services/user-api/src/controllers/stockController.ts
+++ services/user-api/src/controllers/stockController.ts
@@ -1,42 +1,8 @@
-import {
-  type GetStockPricesRequest,
-  type GetStockPricesResponse,
-} from "shared-types/dtos/order/getStockPrices";
 import type { ContextWithUser } from "shared-types/hono";
 import { handleError } from "shared-utils";
-import { makeInternalRequest } from "shared-utils/internalCommunication";
 import stockService from "../services/stockService";
 
 const stockController = {
-  getStockPrices: async (c: ContextWithUser) => {
-    try {
-      const response = await makeInternalRequest<GetStockPricesRequest, GetStockPricesResponse>({
-        body: undefined,
-      })("orderService", "getStockPrices");
-      if (!response.success || !response.data.success) {
-        return handleError(
-          c,
-          new Error("Failed to get stock prices"),
-          "Failed to get stock prices",
-          400
-        );
-      }
-      const sortedData = response.data.data.sort((stock1, stock2) => {
-        const stockName1Upper = stock1.stock_name.toUpperCase();
-        const stockName2Upper = stock2.stock_name.toUpperCase();
-        if (stockName1Upper < stockName2Upper) {
-          return 1;
-        }
-        if (stockName1Upper > stockName2Upper) {
-          return -1;
-        }
-        return 0;
-      });
-      return c.json({ success: true, data: sortedData });
-    } catch (e) {
-      return handleError(c, e, "Failed to get stock prices", 500);
-    }
-  },
   getStockPortfolio: async (c: ContextWithUser) => {
     const { username } = c.get("user");
     try {
